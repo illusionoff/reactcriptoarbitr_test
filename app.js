ee53ccb3a5997e7c667f8e81d0f68ddb430fa8b8
illusionoff <venom12@inbox.ru>
2021-08-15 21:16:59 +0300

    scales color search

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const app = express();
 
 app.use(express.json({ extended: true }));
 // app.use(express.urlencoded({ extended: false }));
-const PORT = config.get('port') || 5000;
+const PORT = config.get('PORT') || 5000;
 
-app.use('/api/message', require('./routes/routerMessage'));
+// app.use('/api/message', require('./routes/routerMessage'));
 // app.use('/', require('./routes/routerIndex')); dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
 if (process.env.NODE_ENV === 'production') {
   // app.use('/', express.static(path.join(__dirname, 'client', 'build')));
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === 'production') {
 } else {
   console.log("development mode");
 }
-
+config.has();
 async function start() {
   try {
     app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`));
